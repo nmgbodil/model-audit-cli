e4e65f7eddbbcb99200471c78a8cbd6c2d2ea1d0
willott29 <143354138+willott29@users.noreply.github.com>
2025-09-16 13:36:19 -0400

    NDJSON Files

---
+++ src/model_audit_cli/models.py
@@ -0,0 +1,46 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field, StringConstraints
+
+
+class SizeScore(BaseModel):
+    """Calculates the score for size metric."""
+    raspberry_pi: float = Field(..., ge=0, le=1)
+    jetson_nano: float = Field(..., ge=0, le=1)
+    desktop_pc: float = Field(..., ge=0, le=1)
+    aws_server: float = Field(..., ge=0, le=1)
+
+
+class Metrics(BaseModel):
+    """Base model for the metrics calculations."""
+    name: str
+    category: Annotated[
+        str, StringConstraints(pattern="^(MODEL|DATASET|CODE)$")
+    ]  # enum-like restriction
+
+    net_score: float = Field(..., ge=0, le=1)
+    net_score_latency: int
+
+    ramp_up_time: float = Field(..., ge=0, le=1)
+    ramp_up_time_latency: int
+
+    bus_factor: float = Field(..., ge=0, le=1)
+    bus_factor_latency: int
+
+    performance_claims: float = Field(..., ge=0, le=1)
+    performance_claims_latency: int
+
+    license: float = Field(..., ge=0, le=1)
+    license_latency: int
+
+    size_score: SizeScore
+    size_score_latency: int
+
+    dataset_and_code_score: float = Field(..., ge=0, le=1)
+    dataset_and_code_score_latency: int
+
+    dataset_quality: float = Field(..., ge=0, le=1)
+    dataset_quality_latency: int
+
+    code_quality: float = Field(..., ge=0, le=1)
+    code_quality_latency: int
