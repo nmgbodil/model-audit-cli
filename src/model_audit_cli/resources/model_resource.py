b225bf69d0d7923d8cfc3136f9ce40a0a264fd74
Noddie Mgbodille <nmgbodille@gmail.com>
2025-09-25 14:07:08 -0400

    Implemented file reading for models

--- src/model_audit_cli/resources/model_resource.py
+++ src/model_audit_cli/resources/model_resource.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from model_audit_cli.adapters.model_fetchers import HFModelFetcher
+from model_audit_cli.errors import NOT_FOUND, AppError
 from model_audit_cli.resources.base_resource import _BaseResource
 
 
@@ -22,13 +24,55 @@ class ModelResource(_BaseResource):
         """
         pass
 
-    def open_file(self, filename: str) -> Any:
-        """Open a file within the model resource.
+    def open_file(self, filename: str) -> str:
+        """Opens and reads the content of a file from a specified repository.
 
         Args:
-            filename (str): The name of the file to open.
+            filename (str): The name of the file to be opened and read.
 
         Returns:
-            Any: The contents of the file.
+            str: The content of the file as a string.
+
+        Raises:
+            AppError: If the file does not exist in the repository, an error is raised
+                      with a NOT_FOUND status and additional context information.
         """
-        pass
+        text = None
+        with HFModelFetcher(self._repo_id) as model:
+            if model.exists(filename):
+                text = model.read_text(filename)
+
+        if text is not None:
+            return text
+
+        raise AppError(
+            NOT_FOUND,
+            f"{filename} does not exist in this repo.",
+            context={"url": self.url},
+        )
+
+    def open_json_file(self, filename: str) -> Any:
+        """Opens and reads a JSON file from a specified repository.
+
+        Args:
+            filename (str): The name of the JSON file to be opened.
+
+        Returns:
+            Any: The data read from the JSON file.
+
+        Raises:
+            AppError: If the specified file does not exist in the repository.
+        """
+        data = None
+        with HFModelFetcher(self._repo_id) as model:
+            if model.exists(filename):
+                data = model.read_json(filename)
+
+        if data is not None:
+            return data
+
+        raise AppError(
+            NOT_FOUND,
+            f"{filename} does not exist in this repo.",
+            context={"url": self.url},
+        )
