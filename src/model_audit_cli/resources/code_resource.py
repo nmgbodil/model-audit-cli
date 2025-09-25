48b69c879caf0619d4330440248a86d71bb8716a
Noddie Mgbodille <nmgbodille@gmail.com>
2025-09-25 14:28:01 -0400

    Implemented file reading for codebases

--- src/model_audit_cli/resources/code_resource.py
+++ src/model_audit_cli/resources/code_resource.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from model_audit_cli.adapters.code_fetchers import open_codebase
+from model_audit_cli.errors import NOT_FOUND, AppError
 from model_audit_cli.resources.base_resource import _BaseResource
 
 
@@ -15,13 +17,55 @@ class CodeResource(_BaseResource):
         """
         pass
 
-    def open_file(self, filename: str) -> Any:
-        """Opens a file with the given filename.
+    def open_file(self, filename: str) -> str:
+        """Opens and reads the content of a file from a specified repository.
 
         Args:
-            filename (str): The name of the file to be opened.
+            filename (str): The name of the file to be opened and read.
 
         Returns:
-            Any: The content of the file.
+            str: The content of the file as a string.
+
+        Raises:
+            AppError: If the file does not exist in the repository, an error is raised
+                      with a NOT_FOUND status and additional context information.
         """
-        pass
+        text = None
+        with open_codebase(self.url) as code:
+            if code.exists(filename):
+                text = code.read_text(filename)
+
+        if text is not None:
+            return text
+
+        raise AppError(
+            NOT_FOUND,
+            f"{filename} does not exist in this repo.",
+            context={"url": self.url},
+        )
+
+    def open_json(self, filename: str) -> Any:
+        """Opens and reads a JSON file from the codebase.
+
+        Args:
+            filename (str): The name of the JSON file to be opened.
+
+        Returns:
+            Any: The parsed JSON data from the file.
+
+        Raises:
+            AppError: If the specified file does not exist in the repository.
+        """
+        data = None
+        with open_codebase(self.url) as code:
+            if code.exists(filename):
+                data = code.read_json(filename)
+
+        if data is not None:
+            return data
+
+        raise AppError(
+            NOT_FOUND,
+            f"{filename} does not exist in this repo.",
+            context={"url": self.url},
+        )
