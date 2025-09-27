3040b8b7e93861c55fa783522dbc3dc40663be76
Noddie Mgbodille <nmgbodille@gmail.com>
2025-09-27 13:43:24 -0400

    Made dataset resource optional

--- src/model_audit_cli/model.py
+++ src/model_audit_cli/model.py
@@ -11,11 +11,11 @@ class Model(BaseModel):
     """Represents a machine learning model with associated dataset and code resources.
 
     Attributes:
-        dataset (DatasetResource): The dataset resource associated with the model.
         model (ModelResource): The model resource containing model-specific information.
+        dataset (Optional[DatasetResource]): The dataset resource associated with model.
         code (Optional[CodeResource]): The code resource associated with the model.
     """
 
-    dataset: DatasetResource
     model: ModelResource
+    dataset: Optional[DatasetResource] = None
     code: Optional[CodeResource] = None
