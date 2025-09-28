a198676de4451f2a3368caaada2a778b3d77049d
annastarky <228934469+annastarky@users.noreply.github.com>
2025-09-28 14:28:14 -0400

    WIP: Add your metrics here

--- src/model_audit_cli/metrics/registry.py
+++ src/model_audit_cli/metrics/registry.py
@@ -1,8 +1,13 @@
 from .base_metric import Metric
+
+from .bus_factor import BusFactor
+from .code_quality import CodeQuality
 from .ramp_up_time import RampUpTime
 
 ALL_METRICS: list[Metric] = [
     RampUpTime(),
+    BusFactor(),
+    CodeQuality(),
     # add more metrics here
 ]
 
